e869e4b2b1d08e6e872e3470a33d3f6cd4b9549c
mitsbits <mitsbits@gmail.com>
2018-03-05 21:45:13 +0200

    Add project files.

---
+++ signaletest/Hubs/NotificationsHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signaletest.Hubs
+{
+    public class NotificationsHub : Hub
+    {
+        private readonly string[] _args;
+
+        public NotificationsHub(string[] args)
+        {
+            _args = args;
+        }
+
+        public async Task Notify(Notification message)
+        {
+            var tasks = _args.Select(x => Clients.All.InvokeAsync("notify", new Notification() { Description = message.Description, Title = x, Id = 2 }));
+            await Task.WhenAll(tasks);
+        }
+
+        public class Notification
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.UtcNow;
+        }
+    }
+}
